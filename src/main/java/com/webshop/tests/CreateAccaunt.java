2bcc6f81678a39a531047af440f65eaa4f67edc2
Andrey B <t.h.e.k.e.n.n.a.r.t@gmail.com>
2024-12-05 17:45:30 +0700

    update

---
+++ src/main/java/com/webshop/tests/CreateAccaunt.java
@@ -0,0 +1,29 @@
+package com.webshop.tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+
+public class CreateAccaunt extends  TestBase {
+
+    @Test
+    public void registrationPositiveTest(){
+        Random random = new Random();
+        int i = random.nextInt(1000) + 1000;
+        click(By.cssSelector("[href='/register']"));
+        type(By.cssSelector("[name='FirstName']"), "Andrbey");
+        type(By.cssSelector("[name='LastName']"), "Bu");
+        type(By.cssSelector("[name='Email']"), "ggvonbdoy" + i + "@gmail.com");
+        type(By.cssSelector("[name='Password']"), "Iamlovekedy21$");
+        type(By.cssSelector("[name='ConfirmPassword']"), "Iamlovekedy21$");
+
+        click(By.cssSelector("[value='Register']"));
+        Assert.assertTrue(isElementPresent(By.cssSelector("[class='ico-logout']")));
+
+
+
+
+    }
+}
