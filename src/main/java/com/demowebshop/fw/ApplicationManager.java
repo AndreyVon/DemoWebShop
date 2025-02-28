c0cb04df676a4e70b7ba139ce9ed764e20b41cf2
AndreyVon <t.h.e.k.e.n.n.a.r.t@gmail.com>
2025-02-28 14:30:15 +0700

    hg

--- src/main/java/com/demowebshop/fw/ApplicationManager.java
+++ src/main/java/com/demowebshop/fw/ApplicationManager.java
@@ -2,17 +2,35 @@ package com.demowebshop.fw;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
 public class ApplicationManager {
 
     static WebDriver driver;
+    String browser;
     UserHelper user;
     ProductHelper product;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
+
+
     public void init() {
-        driver = new ChromeDriver();
+        if(browser.equalsIgnoreCase("chrome")){
+            driver = new ChromeDriver();
+        }if(browser.equalsIgnoreCase("firefox")){
+            driver = new FirefoxDriver();
+
+        }if(browser.equalsIgnoreCase("edge")){
+            driver = new EdgeDriver();
+        }
+
+
         driver.manage().window().maximize();
         driver.get("https://demowebshop.tricentis.com/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
--- src/test/java/com/ait/qa34/TesteBase.java
+++ src/test/java/com/ait/qa34/TesteBase.java
@@ -1,12 +1,22 @@
 package com.ait.qa34;
 
 import com.demowebshop.fw.ApplicationManager;
+import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
 
 public class TesteBase {
 
-    protected static ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+
+    Logger logger = LoggerFactory.getLogger(TesteBase.class);
 
     // @BeforeMethod
     @BeforeSuite
@@ -16,10 +26,29 @@ public class TesteBase {
     }
 
     // @AfterMethod(enabled = false)
-    @AfterSuite(enabled = false)
+    @AfterSuite()
     public void tearDown(){
         app.stop();
     }
+
+    @BeforeMethod
+    public void startTest(Method method){
+        logger.info("Start Test " + method.getName());
+
+    }
+
+    @AfterMethod
+    public void stopTest(ITestResult result){
+        if(result.isSuccess()){
+            logger.info("PASSED " + result.getMethod().getMethodName());
+        }else {
+            logger.error("FAILED " + result.getMethod().getMethodName()+ " Screenshot: " + app.getUser().takeScreenshot());
+        }
+        logger.info("Stop Test");
+        logger.info("============================================================");
+    }
+
+
 
 
 }
